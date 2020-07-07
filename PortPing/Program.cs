c41a5b72bfe46f3a32e51bba5c0ac5d5e73fd0d7
Robert Adams <robert.adams@abide-financial.com>
2020-07-07 11:49:21 +0100

    Initial implementation.

---
+++ PortPing/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace PortPing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: PortPing [host] [port]");
+                return;
+            }
+
+            var host = args[0];
+            var port = int.Parse(args[1]);
+
+
+            while (true)
+            {
+                try
+                {
+                    using (var client = new TcpClient())
+                    {
+                        client.Connect(host, port);
+                        Console.WriteLine($"Connected to {host}:{port}");
+                    }
+
+                    Thread.Sleep(1000);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Couldn't connect to {host}:{port} - {e.Message}");
+                }
+            }
+        }
+    }
+}
